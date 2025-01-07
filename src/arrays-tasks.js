cd09069702b4d10d94c82fa2ef40347854311d18
Medvedev Kirill <medvedev_k@icloud.com>
2025-01-07 23:47:51 +0500

    feat: implement getTail and doubleArray functions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -212,8 +212,8 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return n < 1 ? [] : arr.slice(-n);
 }
 
 /**
@@ -228,8 +228,8 @@ function getTail(/* arr, n */) {
  *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
  *    doubleArray([]) => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  return arr.concat(arr);
 }
 
 /**
