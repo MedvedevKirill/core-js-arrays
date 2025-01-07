6846d6f7200340678f69d9ce3d67c92015a2f4b6
Medvedev Kirill <medvedev_k@icloud.com>
2025-01-07 20:48:13 +0500

    feat: implement sumArrays and findElement functions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -40,8 +40,20 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let bigArray;
+  let smallArray;
+  if (arr1 > arr2) {
+    bigArray = arr1;
+    smallArray = arr2;
+  } else {
+    bigArray = arr2;
+    smallArray = arr1;
+  }
+  const result = smallArray.map((value, index) => {
+    return value + bigArray[index];
+  });
+  return [...result, ...bigArray.slice(smallArray.length)];
 }
 
 /**
@@ -56,8 +68,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
