a69a752cf80162708ce9c10958359f5188686d49
Medvedev Kirill <medvedev_k@icloud.com>
2025-01-08 20:03:24 +0500

    feat: implement getElementByIndices, getFalsyValuesCount and getIdentityMatrix functions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -389,8 +389,8 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.reduce((prev, value) => prev[value], arr);
 }
 
 /**
@@ -405,8 +405,8 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((value) => !value).length;
 }
 
 /**
@@ -427,8 +427,10 @@ function getFalsyValuesCount(/* arr */) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (v, k) =>
+    Array.from({ length: n }, (value, index) => (index === k ? 1 : 0))
+  );
 }
 
 /**
